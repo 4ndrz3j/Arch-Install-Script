63ccc2ae04485c10a1d9ba60c6f556d65d54e165
4ndrz3j <70114673+4ndrz3j@users.noreply.github.com>
2024-01-07 22:47:24 +0100

    Update install_arch.sh

--- install_arch.sh
+++ install_arch.sh
@@ -26,9 +26,8 @@ PACKAGES="base linux-firmware cryptsetup grub efibootmgr mkinitcpio xterm networ
 
 # Aditional packages for your install.
 
-PACKAGES_RICE="arc-gtk-theme sysstat base-devel zsh xorg unzip i3 git xorg-xinit alacritty neovim flameshot network-manager-applet  feh i3blocks pavucontrol i3status i3-gaps rofi picom python-pip wget xss-lock"
-PACKAGES_UTILITIES="signal-desktop"
-PACKAGES_OPTIONAL="dunst papirus-icon-theme pulseaudio-bluetooth lxappearance-gtk3 qemu virt-manager virt-viewer dnsmasq vde2 bridge-utils openbsd-netcat"
+PACKAGES_RICE="arc-gtk-theme sysstat base-devel zsh xorg unzip i3 git xorg-xinit alacritty network-manager-applet neovim feh i3blocks pavucontrol i3status i3-gaps rofi picom python-pip wget xss-lock"
+PACKAGES_OPTIONAL="signal-desktop  flameshot dunst papirus-icon-theme pulseaudio-bluetooth lxappearance-gtk3 qemu virt-manager virt-viewer dnsmasq vde2 bridge-utils openbsd-netcat"
 
 # Driver for GPU
 # See here available drivers
@@ -112,7 +111,7 @@ encrypt_disk(){
 # Change Kernel to desired version
 chroot_and_install(){
     echo -e "${BB}Installing packages now${CR}"
-    pacstrap /mnt $KERNEL $PACKAGES $PACKAGES_OPTIONAL $PACKAGES_UTILITIES $PACKAGES_RICE $GPU_DRIVER
+    pacstrap /mnt $KERNEL $PACKAGES $PACKAGES_UTILITIES $PACKAGES_RICE $GPU_DRIVER
     echo -e "${BB}Generating fstab${CR}"
     genfstab -U /mnt >> /mnt/etc/fstab
     sed -i "s/relatime/relatime,discard/g" /mnt/etc/fstab
