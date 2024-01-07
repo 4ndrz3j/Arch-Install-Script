bb5177a7ba616c96bb237c33ae4e504a0700d9dc
4ndrz3j <70114673+4ndrz3j@users.noreply.github.com>
2024-01-07 22:55:31 +0100

    Add chromium

--- install_arch.sh
+++ install_arch.sh
@@ -27,7 +27,7 @@ PACKAGES="base linux-firmware cryptsetup grub efibootmgr mkinitcpio xterm networ
 # Aditional packages for your install.
 
 PACKAGES_RICE="arc-gtk-theme sysstat base-devel zsh xorg unzip i3 git xorg-xinit alacritty network-manager-applet neovim feh i3blocks pavucontrol i3status i3-gaps rofi picom python-pip wget xss-lock"
-PACKAGES_OPTIONAL="signal-desktop  flameshot dunst papirus-icon-theme pulseaudio-bluetooth lxappearance-gtk3 qemu virt-manager virt-viewer dnsmasq vde2 bridge-utils openbsd-netcat"
+PACKAGES_OPTIONAL="signal-desktop chromium flameshot dunst papirus-icon-theme pulseaudio-bluetooth lxappearance-gtk3 qemu virt-manager virt-viewer dnsmasq vde2 bridge-utils openbsd-netcat"
 
 # Driver for GPU
 # See here available drivers
